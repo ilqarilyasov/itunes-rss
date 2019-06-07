dc4ebd4dbdc8de9533b55273aecad0fe03ee01b7
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-06-07 18:59:52 -0500

    Add a custom table view cell with a Subtitle cell style

---
+++ RSS/ResultTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ResultTableViewCell.swift
+//  RSS
+//
+//  Created by Ilgar Ilyasov on 6/7/19.
+//  Copyright © 2019 IIIyasov. All rights reserved.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}

6bd8243f8054c6810da1b6463a72061efca51e94
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-06-08 13:26:20 -0500

    Make Result model object class to refer it later in Image Loader

--- RSS/Model/Result.swift
+++ RSS/Model/Result.swift
@@ -6,25 +6,29 @@
 //  Copyright © 2019 IIIyasov. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-struct Result: Decodable, Equatable {
+class Result: Decodable, Equatable {
     let artistName: String
     let name: String
     let kind: Kind
     let artworkUrl100: String
-    var imageData: Data? = nil
+    var image: UIImage? = nil
     
     enum ResultCodingKeys: String, CodingKey {
         case artistName, name, kind, artworkUrl100
     }
     
-    init(from decoder: Decoder) throws {
+    required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: ResultCodingKeys.self)
         self.artistName = try container.decode(String.self, forKey: .artistName)
         self.name = try container.decode(String.self, forKey: .name)
         let kind = try container.decode(String.self, forKey: .kind)
         self.kind = Kind(rawValue: kind) ?? .album
         self.artworkUrl100 = try container.decode(String.self, forKey: .artworkUrl100)
+    }
+    
+    static func == (lhs: Result, rhs: Result) -> Bool {
+        return lhs.artworkUrl100 == rhs.artworkUrl100 
     }
 }
